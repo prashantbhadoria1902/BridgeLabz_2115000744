c68e68b4c444a450573c17ae880de76467e7c1ab
prashantbhadoria1902 <prashantbhadoria1902@gmail.com>
2025-02-02 23:53:34 +0530

    [Prashant] Add . Implemented classes with appropriate constructors, access modifiers, and methods

---
+++ Level 2/Vehicle.java
@@ -0,0 +1,32 @@
+
+class Vehicle {
+    String ownerName;
+    String vehicleType;
+    static double registrationFee = 150.0;
+
+    Vehicle(String ownerName, String vehicleType) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+    }
+
+    void displayVehicleDetails() {
+        System.out.println("Owner Name: " + ownerName);
+        System.out.println("Vehicle Type: " + vehicleType);
+        System.out.println("Registration Fee: " + registrationFee);
+    }
+
+    static void updateRegistrationFee(double newRegistrationFee) {
+        registrationFee = newRegistrationFee;
+    }
+
+    public static void main(String[] args) {
+        Vehicle vehicle1 = new Vehicle("Prashant", "Car");
+        Vehicle vehicle2 = new Vehicle("Shivangi", "Motorcycle");
+        vehicle1.displayVehicleDetails();
+        vehicle2.displayVehicleDetails();
+        Vehicle.updateRegistrationFee(200.0);
+        vehicle1.displayVehicleDetails();
+        vehicle2.displayVehicleDetails();
+    }
+}
+
