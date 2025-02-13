fff8463c9b74498859b6660293aee8cfbdec5766
prashantbhadoria1902 <prashantbhadoria1902@gmail.com>
2025-02-13 11:38:39 +0530

    [Prashant] Added QueueUsingStacks.java file

---
+++ Stack and Queue/QueueUsingStacks.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+class QueueUsingStacks {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    void enqueue(int x) {
+        s1.push(x);
+    }
+
+    int dequeue() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.isEmpty() ? -1 : s2.pop();
+    }
+
+    public static void main(String[] args) {
+        QueueUsingStacks queue = new QueueUsingStacks();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+    }
+}
