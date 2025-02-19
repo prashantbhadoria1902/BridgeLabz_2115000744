38f9c32e6152ac993c9f4f6f8cea3f9aa6548426
prashantbhadoria1902 <prashantbhadoria1902@gmail.com>
2025-02-19 11:03:47 +0530

    [Prashant] Added Set Interface assignment

---
+++ Set Interface/UnionIntersection.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class UnionIntersection {
+    public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new HashSet<>(set1);
+        result.addAll(set2);
+        return result;
+    }
+
+    public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new HashSet<>(set1);
+        result.retainAll(set2);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(3, 4, 5));
+        System.out.println("Union: " + union(set1, set2));
+        System.out.println("Intersection: " + intersection(set1, set2));
+    }
+}
